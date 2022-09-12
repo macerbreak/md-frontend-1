66366717335e6d4827f540ace5005284402d8815
Volodymyr Savko <v.savko@dinamicka.com>
2022-09-12 23:36:33 +0300

    getting all stations

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Box, styled} from "@mui/material";
 import {Route, Routes} from "react-router-dom";
 import DashboardPollutionMap from "./DashboardPollutionMap";
 import Sidebar from "./Sidebar/Sidebar";
 import {constants} from "../../system/constants";
+import axios from "axios";
 
+const AQI_ACCESS_TOKEN = process.env.REACT_APP_AQI_ACCESS_TOKEN
 const Dashboard = () => {
+
+    useEffect(()=>{
+        axios
+            // .get(`https://api.waqi.info/feed/ukraine?token=${AQI_ACCESS_TOKEN}`)
+            .get(`https://waqi.info/rtdata/markers-${+Date.now().toString().slice(0,10)}/000.json`)
+            // .get("https://waqi.info/rtdata/markers-1663013315/000.json")
+            .then((res)=>{
+                console.log({res})
+            })
+    },[])
     return <>
     <Box sx={{
         width:"100%",
